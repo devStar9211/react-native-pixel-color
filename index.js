07712e24dc1d1f012f15c913d07887801871920e
devStar9211 <32103851+devStar9211@users.noreply.github.com>
2018-06-11 22:28:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ import {
   NativeModules,
 } from 'react-native';
 
-export const getHex = (path, options) => new Promise((resolve, reject) => {
-  NativeModules.RNPixelColor.getHex(path, options, (err, color) => {
+export const getHex = (options) => new Promise((resolve, reject) => {
+  NativeModules.RNPixelColor.getHex(options, (err, color) => {
     if (err) return reject(err);
 
     resolve(color);
   });
 });
 
+export const createTempImage = (path) => NativeModules.RNPixelColor.createTempImage(path);
 export default {
   getHex,
+  createTempImage,
 }
